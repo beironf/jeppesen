6595682da80b3f482c55d110d4ad51a96bf071ed
simstra <simon.strandberg@live.se>
2017-11-14 17:32:38 +0100

    not finished. Forbids mandatory segs...

--- create_forbid_segments.py
+++ create_forbid_segments.py
@@ -2,17 +2,31 @@
 
 import parse_data
 
-graph = parse_data.createGraph()
+segments = parse_data.getSegments()
+possible_destinations = parse_data.getPossibleDestinations(segments)
 allowed_routes = parse_data.getAllowedRoutes()
-entry_nodes = parse_data.getEntryNodes(allowed_routes)
+points = parse_data.getPoints(segments)
+routes_for_all_entry_nodes = parse_data.getRoutesForAllEntryNodes(allowed_routes)
 
-for entry_node in entry_nodes:
-    #Create list with all routes originating in entry_node
-    current_routes = []
-    for route in allowed_routes:
-        if route[0][0] == entry_node:
-            #We are now looking at a route with 'node' as its starting node
-            current_routes.append(route)
-    #Now current_routes contains several lists, each with a route starting from
-    #entry_node
+forbidden_segs = {}
 
+for entry_node in routes_for_all_entry_nodes:
+    forbidden_tmp = []
+    current_routes = routes_for_all_entry_nodes[entry_node]
+    for route in current_routes:
+        for seg in route:
+            used_segs = []
+            for rt in current_routes:
+                for sg in rt:
+                    if sg['from'] == seg['from'] and not(sg in used_segs):
+                        used_segs.append(sg)
+            possible_segs = []
+            for sg in segments:
+                if sg['from'] == seg['from']:
+                    possible_segs.append(sg)
+            for sg in possible_segs:
+                if not(sg in used_segs and sg in forbidden_tmp):
+                    forbidden_tmp.append(sg)
+    forbidden_segs[entry_node] = forbidden_tmp
+
+print(forbidden_segs['WPT INTIK'])
