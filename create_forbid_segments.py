d3f7eda3b29ae18c2b94e17804205c97413dfbe1
simstra <simon.strandberg@live.se>
2017-11-15 14:14:48 +0100

    changed create_forbid_segs.py to function holder file

--- create_forbid_segments.py
+++ create_forbid_segments.py
@@ -13,26 +13,25 @@ routes_by_entry_node = parse_data.getRoutesForAllEntryNodes(allowed_routes)
 
 forbidden_segs = {}
 
-for entry_node in routes_by_entry_node:
-    forbidden_tmp = []
-    current_routes = routes_by_entry_node[entry_node]
-    for route in current_routes:
-        for seg in route:
-            used_segs = []
-            for rt in current_routes:
-                for sg in rt:
-                    if sg['from'] == seg['from'] and not(sg in used_segs):
-                        used_segs.append(sg)
-            possible_segs = []
-            for sg in segments:
-                if sg['from'] == seg['from']:
-                    possible_segs.append(sg)
-            for sg in possible_segs:
-                if (sg not in used_segs) and (sg not in forbidden_tmp):
-                        forbidden_tmp.append(sg)
-    forbidden_segs[entry_node] = forbidden_tmp
+def createForbiddenSegments():
 
-#print(possible_segs)
-#print(used_segs)
-#print(forbidden_tmp)
-#print(forbidden_segs['WPT INTIK'])
+    for entry_node in routes_by_entry_node:
+        forbidden_tmp = []
+        current_routes = routes_by_entry_node[entry_node]
+        for route in current_routes:
+            for seg in route:
+                used_segs = []
+                for rt in current_routes:
+                    for sg in rt:
+                        if sg['from'] == seg['from'] and not(sg in used_segs):
+                            used_segs.append(sg)
+                possible_segs = []
+                for sg in segments:
+                    if sg['from'] == seg['from']:
+                        possible_segs.append(sg)
+                for sg in possible_segs:
+                    if (sg not in used_segs) and (sg not in forbidden_tmp):
+                            forbidden_tmp.append(sg)
+        forbidden_segs[entry_node] = forbidden_tmp
+    
+    return forbidden_segs
