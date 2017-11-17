d117ad9247110c5f6e55d3b882c88bcafd29a8c6
FKjernald <f.kjernald@gmail.com>
2017-11-17 17:25:15 +0100

    changes 17/11

--- confirm_solution.py
+++ confirm_solution.py
@@ -1,48 +1,48 @@
 
 class ConfirmSolution(object):
-	def __init__(self, forbidden_segs, forbidden_entry_segs, segments, points, entry_nodes, exit_nodes):
+    def __init__(self, forbidden_segs, forbidden_entry_segs, segments, points, entry_nodes, exit_nodes):
 
-		self.forbidden_segs = forbidden_segs
-		self.forbidden_entry_segs = forbidden_entry_segs
-		self.segments = segments
-		self.points = points
-		self.entry_nodes = entry_nodes
-		self.possible_routes = []
-		self.exit_nodes = exit_nodes
+        self.forbidden_segs = forbidden_segs
+        self.forbidden_entry_segs = forbidden_entry_segs
+        self.segments = segments
+        self.points = points
+        self.entry_nodes = entry_nodes
+        self.possible_routes = []
+        self.exit_nodes = exit_nodes
 
-	def isForbidden(self, seg, entry_seg):
-		if entry_seg['from'] in self.entry_nodes:
-			if seg in self.forbidden_segs[entry_seg['from']]:
-				return True
-		else:
-			return False
+    def isForbidden(self, seg, entry_seg):
+        if entry_seg['from'] in self.entry_nodes:
+            if seg in self.forbidden_segs[entry_seg['from']]:
+                return True
+        else:
+            return False
 
-	def getSegments(self, fr):
-		segs = [seg for seg in self.segments if seg['from']==fr]
-		return segs
+    def getSegments(self, fr):
+        segs = [seg for seg in self.segments if seg['from']==fr]
+        return segs
 
-	def find_path(self, seg, used_segs):
-		node = seg['to']
+    def find_path(self, seg, used_segs):
+        node = seg['to']
 
-		if (node in self.exit_nodes or self.points[node]['area'][0]!='Z'):
-			self.possible_routes.append(used_segs)
-		
-		else:
-			for sg in self.getSegments(node):
-				if not(self.isForbidden(sg, used_segs[0])) and self.points[node]['area'][0] == 'Z':
-					used_segs.append(sg)
-					self.find_path(sg, used_segs)
-
-
-	def getPossibleRoutes(self):
-		start_segs = []
+        if (node in self.exit_nodes or self.points[node]['area'][0]!='Z'):
+            self.possible_routes.append(used_segs)
         
-		[start_segs.extend(self.getSegments(node)) for node in self.entry_nodes]
-		
-		for start_seg in start_segs:
-			used_segs = [start_seg]
-			if not(self.isForbidden(start_seg, used_segs[0])):
-				self.find_path(start_seg, used_segs)
+        else:
+            for sg in self.getSegments(node):
+                if not(self.isForbidden(sg, used_segs[0])) and self.points[node]['area'][0] == 'Z':
+                    used_segs.append(sg)
+                    self.find_path(sg, used_segs)
 
-		return self.possible_routes
+
+    def getPossibleRoutes(self):
+        start_segs = []
+        
+        [start_segs.extend(self.getSegments(node)) for node in self.entry_nodes]
+        
+        for start_seg in start_segs:
+            used_segs = [start_seg]
+            if not(self.isForbidden(start_seg, used_segs[0])):
+                self.find_path(start_seg, used_segs)
+
+        return self.possible_routes
 
