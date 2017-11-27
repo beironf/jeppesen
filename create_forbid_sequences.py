c60d4817b95874e2f5717d40acbab276fb6235fb
beironf <fredrik@beiron.se>
2017-11-27 16:47:31 +0100

    added some forbidden_entry_nodes

--- create_forbid_sequences.py
+++ create_forbid_sequences.py
@@ -2,61 +2,6 @@
 #The program checks all mandatory routes, grouped by entry point to China, and 
 #finds the set of segments and sequences that need to be forbidden so that only the mandatory
 #routes are possible to choose. 
-def readFile(path):
-    with open(path,'r') as f:
-        return f.read()
-
-def getForbiddenSequences2(routes_by_entry_node, segments, allowed_routes):
-    srtm_lines = readFile('SRTM.txt')
-    srtm_lines = [seq.replace(']','').replace(',\n','').strip() for seq in srtm_lines.split('[')][1:]
-    srtm = []
-    srtm_entry = []
-    for seq in srtm_lines:
-        tmp_list = seq.split('    ')
-        tmp_seq = []
-        
-        for i,l in (enumerate(tmp_list)):
-            if i == 0:
-                node = l.split(': ')
-                srtm_entry.append(node[2][1:-7]) 
-            else:
-                node = l.split(': ')
-                seg_tmp = {'airway': node[1][1:-9], 'from': node[2][1:-7], 'to': node[3][1:-2]}
-                tmp_seq.append(seg_tmp)
-        srtm.append(tmp_seq)
-    #forbidden_seqs = srtm
-    forbidden_seqs = {}
-
-    for entry_node in routes_by_entry_node:
-        current_routes = routes_by_entry_node[entry_node]
-
-        #Find all used segments
-        used_segs = []
-        for route in current_routes:
-            for seg in route:
-                if seg not in used_segs:
-                    used_segs.append(seg)
-
-        #Find all possible segments
-        possible_segs = []
-        for used_seg in used_segs:
-            for possible_seg in segments:
-                if possible_seg['from'] == used_seg['from']:
-                    possible_segs.append(possible_seg)
-
-        #Find which segments to forbid
-        forbidden_tmp = []
-        for seg in possible_segs:
-            if (seg not in used_segs) and (seg not in forbidden_tmp):
-                forbidden_tmp.append(seg)
-
-        for i,entry_point in enumerate(srtm_entry):
-            if entry_point == entry_node:
-                forbidden_tmp.append(srtm[i])
-
-        forbidden_seqs[entry_node] = forbidden_tmp
-
-    return forbidden_seqs
 
 def getAllowedSegmentsFrom(fr, allowed_segments):
         segs = [seg for seg in allowed_segments if seg['from']==fr]
@@ -160,17 +105,29 @@ def getForbiddenSequences(routes_by_entry_node, segments, allowed_routes, allowe
 
     return forbidden_seqs
 
-def getForbiddenEntrySegments(allowed_segments, segments, chinese_areas, entry_nodes):
+def getSegmentsTo(to, segments):
+        segs = [seg for seg in segments if seg['to']==to]
+        return segs
+
+def getForbiddenEntrySegments(allowed_segments, segments, chinese_areas, entry_nodes, allowed_nodes):
     forbidden_entry_segs = []
 
+    # Find all segments that goes in to China from another country that doesn't use an entry_node or an allowed route
     for seg in segments:
         fr = seg['from'][-2:]
         to = seg['to'][-2:]
-        if fr not in chinese_areas \
-            and to in chinese_areas \
-            and (seg not in allowed_segments) \
-            and (seg['to'] not in entry_nodes): # if seg is from another country to China
+        if     (fr not in chinese_areas) \
+           and (to in chinese_areas) \
+           and (seg not in allowed_segments) \
+           and (seg['to'] not in entry_nodes):
             forbidden_entry_segs.append(seg)
+
+    # if there are more than just the entry_node outside of China, forbid segments that allowes us to hop on after the entry_node
+    for node in allowed_nodes:
+        if node[-2:] not in chinese_areas and node not in entry_nodes:
+            for seg in getSegmentsTo(node, segments):
+                if seg not in allowed_segments:
+                    forbidden_entry_segs.append(seg)
 
     return forbidden_entry_segs
 
